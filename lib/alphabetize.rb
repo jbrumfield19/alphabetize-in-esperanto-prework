bd0c0c8de7e19ef91f04a4093575efc0da0fb0d9
Jessica Brumfield <jbrumfield1999@gmail.com>
2019-02-08 13:41:45 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,11 @@
+
 def alphabetize(arr)
   esperantobet=" abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
   arr.sort_by {|arr| arr.split('').map{|char|esperantobet.index(char)}}
 
+<<<<<<< HEAD
 end
+=======
+end
+>>>>>>> e0b631938e2a1148c5e2620ee9858824ec867281
